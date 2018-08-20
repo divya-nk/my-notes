1f374b6a4e0cc852132d07b8f804b5569dcd0bbe
Divya Kannan <mnavane@clemson.edu>
2018-08-20 10:09:20 -0400

    cleaning up -comprehensible output

--- profanity-checker.py
+++ profanity-checker.py
@@ -3,7 +3,13 @@ import urllib
 def profanity_checker(text_to_check):
   connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
   output = connection.read()
-  print(output)
+  #print(output)
+  if "true" in output:
+    print("Profanity Alert!!")
+  elif "false" in output:
+    print("This document has no curse words!")
+  else:
+    print("Unable to scan the document.")
   connection.close()
 
 def read_text():
