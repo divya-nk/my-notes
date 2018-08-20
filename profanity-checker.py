8e8592290cc08a48abcc27f7d39e8b2c54e94492
Divya Kannan <mnavane@clemson.edu>
2018-08-20 10:03:25 -0400

    update with wdyl helper

--- profanity-checker.py
+++ profanity-checker.py
@@ -1,7 +1,16 @@
+import urllib
+
+def profanity_checker(text_to_check):
+  connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
+  output = connection.read()
+  print(output)
+  connection.close()
+
 def read_text():
   f = open("path\to\file\containing\text")
   contents = f.read()
   print(contents)
   f.close()
-  
+  profanity_checker(contents)
+    
 read_text()
