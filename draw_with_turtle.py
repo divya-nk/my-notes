20e53e6fa82be33ea4ff0a293e2c266b2b1ece97
Divya Kannan <mnavane@clemson.edu>
2018-08-19 23:50:01 -0400

    cleaned up functions

--- draw_with_turtle.py
+++ draw_with_turtle.py
@@ -1,24 +1,29 @@
 import turtle
 
-def draw_square():
-  window = turtle.Screen()
-  window.bgcolor("red")
-
-  brad = turtle.Turtle()
-  brad.shape("turtle")
-  brad.color("yellow")
-  brad.speed(2)
-
+def draw_square(turtle_name):  
   #drawing square
   for i in range(4):
     brad.forward(100)
     brad.right(90)
     
+    
+draw_with_turtle():
+  window = turtle.Screen()
+  window.bgcolor("red")
+  
+  # create brad for drawing square
+  brad = turtle.Turtle()
+  brad.shape("turtle")
+  brad.color("yellow")
+  brad.speed(2)
+  draw_square(brad)
+  
+  # create angie for drawing cicle
   angie = turtle.Turtle()
   angie.shape("arrow")
   angie.color("blue")
   angie.circle(100)
-
+  
   window.exitonclick()
-
-draw_square()
+  
+draw_with_turtle()
