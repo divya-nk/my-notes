2d5aff49379e8414b414567bd4b043add3e7c91f
Divya Kannan <mnavane@clemson.edu>
2018-08-19 22:37:28 -0400

    Create take-a-break.py

---
+++ take-a-break.py
@@ -0,0 +1,8 @@
+import time
+import webbrowser
+count =0
+while count <3:
+  time.sleep(3)
+  print("It's break time!")
+  webbrowser.open("https://www.youtube.com/watch?v=nDq6TstdEi8&t=675s")
+  count+=1
